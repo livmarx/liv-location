cf50af0456cd844e9a853e7ec6bf618578e42e9f
Olivia Ehrenreich Marks <oehrenmarks@gmail.com>
2019-05-13 13:58:16 -0400

    Olivia g map (#2)
    * Create components/home/GMap component
    
    * Get rid of styling from create vue
    
    * Delete HelloWorld component and us GMap and home component instead
    
    * Add Goople Map script
    
    * Render Google Map in GMap component
    
    * Change starting location of map

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="map">
+    <div class="google-map" id="map"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GMap',
+  data() {
+    return {
+      lat: 40.8,
+      lgn: -74,
+    };
+  },
+  methods: {
+    renderMap() {
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: this.lat, lng: this.lgn },
+        zoom: 9,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false,
+      });
+    },
+  },
+  mounted() {
+    this.renderMap();
+  },
+};
+</script>
+
+<style>
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
