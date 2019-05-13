df0545667cfcd13a1f328dce8ef1b563a7fe9b3f
Liv Marx <oehrenmarks@gmail.com>
2019-05-13 12:57:32 -0400

    Add .eslinrc

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple">
+      <div class="container">
+        <a href="" class="brand-logo">GeoLiv</a>
+      </div>
+    </nav>
+  </div>
+</template>
