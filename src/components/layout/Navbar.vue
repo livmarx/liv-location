6790a0b406f452b503a0a472a0f254293c86f4ec
Liv Marx <oehrenmarks@gmail.com>
2019-05-13 13:02:47 -0400

    Set up perliminary Navbar component

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,8 +2,24 @@
   <div class="navbar">
     <nav class="deep-purple">
       <div class="container">
-        <a href="" class="brand-logo">GeoLiv</a>
+        <a href="" class="brand-logo left">GeoLiv</a>
+        <ul class="right">
+          <li><a href="">Sign Up</a></li>
+          <li><a href="">Log In</a></li>
+        </ul>
       </div>
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
