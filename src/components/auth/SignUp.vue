c726204a9edf6bfc5fb92760e6ca6865c4869302
Liv Marx <oehrenmarks@gmail.com>
2019-05-13 14:04:51 -0400

    Create SIgnUp component in components/auth/

---
+++ src/components/auth/SignUp.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+    <div class="signup">
+    <h3>Signup</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SighUp',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
