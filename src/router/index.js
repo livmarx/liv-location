2acd97099dd4e57a5a4264968005e65a266ad95f
Liv Marx <oehrenmarks@gmail.com>
2019-05-14 11:35:50 -0400

    Add frontend LogIn route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import GMap from '@/components/home/GMap';
 import SignUp from '@/components/auth/SignUp.vue';
+import LogIn from '@/components/auth/LogIn.vue';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/signup',
       name: 'SignUp',
       component: SignUp,
+    },
+    {
+      path: '/login',
+      name: 'LogIn',
+      component: LogIn,
     },
   ],
 });
