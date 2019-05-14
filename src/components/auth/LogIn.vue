9f33029869beadf15c0636e4ab518f7ea4b8e45b
Liv Marx <oehrenmarks@gmail.com>
2019-05-14 11:22:09 -0400

    Create LogIn component

---
+++ src/components/auth/LogIn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="lingin container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
+
+
