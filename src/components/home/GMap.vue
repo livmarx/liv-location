b58d00b1f5f97a3bd77d302556ef09d645cafc16
Liv Marx <oehrenmarks@gmail.com>
2019-05-14 12:21:41 -0400

    Use navigator object to get location of user during mounted lifecycle hook in GMap component

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -10,23 +10,44 @@ export default {
   name: 'GMap',
   data() {
     return {
-      lat: 40.8,
-      lgn: -74,
+      lat: 20.8,
+      lgn: -100.4,
     };
   },
   methods: {
     renderMap() {
       const map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: this.lat, lng: this.lgn },
-        zoom: 9,
-        maxZoom: 15,
+        zoom: 15,
+        maxZoom: 20,
         minZoom: 3,
         streetViewControl: false,
       });
     },
   },
   mounted() {
-    this.renderMap();
+    // get logged in user geolocation:
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log('POSTION: ', position);
+          this.lat = position.coords.latitude;
+          this.lgn = position.coords.longitude;
+          this.renderMap();
+        },
+        err => {
+          console.error(err);
+          this.renderMap();
+        },
+        {
+          maximumAge: 60000,
+          timeout: 3000,
+        }
+      );
+    } else {
+      // Position centered by default (hard coded) values
+      this.renderMap();
+    }
     console.log('CURRENT USER: ', firebase.auth().currentUser);
   },
 };
